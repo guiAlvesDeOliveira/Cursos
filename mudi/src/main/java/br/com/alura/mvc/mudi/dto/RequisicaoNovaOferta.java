a08aace812d19cc377855a9e0168a85bc5a85fc0
Guilherme Alves <guialves.oliveira1234@gmail.com>
2022-04-26 00:27:35 -0300

    versão final da aplicação em spring

--- mudi/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -14,12 +14,12 @@ public class RequisicaoNovaOferta {
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 	private Long pedidoId;
-	
+
 	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")
 	@NotNull
 	private String valor;
 	
-	@Pattern(regexp = "\\{2}/\\d{2}/\\d{4}$")
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
 	@NotNull
 	private String dataDaEntrega;
 	
